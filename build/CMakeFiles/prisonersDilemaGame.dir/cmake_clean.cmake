00034921d06e0e37bfad5eb0dfca28a63cd0a64d
alex <alexbr1208@gmail.com>
2025-05-07 09:29:08 -0400

    v0.5b - Removed unnecessary IDE configuration files and dependencies.

---
+++ build/CMakeFiles/prisonersDilemaGame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prisonersDilemaGame.dir/src/main.cpp.o"
+  "CMakeFiles/prisonersDilemaGame.dir/src/main.cpp.o.d"
+  "prisonersDilemaGame"
+  "prisonersDilemaGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prisonersDilemaGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+#else
 #include <GL/glut.h>
+#endif
 #include <iostream>
 #include <random>
 #include <ctime>
